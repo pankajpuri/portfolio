c20527aeb2cdd95923207ed07003abb7d9110c4a
Pankaj Puri <pankajpuri016@gmail.com>
2023-03-23 16:02:28 +1300

    change image directory to docs

--- src/components/profileSection.jsx
+++ src/components/profileSection.jsx
@@ -18,7 +18,7 @@ function ProfileSection({ name, role }) {
           >
             <img
               className="`img-fluid w-100` profileImageBorder"
-              src="../assests/images/profile.jpg"
+              src="../docs/images/profile.jpg"
               alt="profile"
             ></img>
           </div>
@@ -39,7 +39,7 @@ function ProfileSection({ name, role }) {
             <div className="cvBtnPlaySection">
               <Fade bottom>
                 <a
-                  href="./assests/images/Pankaj_Puri_CV.pdf"
+                  href="./docs/images/Pankaj_Puri_CV.pdf"
                   className=" text-white btn border rounded-pill btnStyle"
                   style={{
                     display: "flex",
@@ -77,7 +77,7 @@ function ProfileSection({ name, role }) {
                           controls="controls"
                         >
                           <source
-                            src="./assests/videos/aboutyourself.mp4"
+                            src="./docs/videos/aboutyourself.mp4"
                             type="video/mp4"
                           ></source>
                         </video>
--- src/components/services/experienceData.js
+++ src/components/services/experienceData.js
@@ -2,7 +2,7 @@ export const experienceData = [
   {
     id: "_1",
     title: "Front End - Tesla Clone",
-    landingImage: "../assests/images/teslaImg.png",
+    landingImage: "../docs/images/teslaImg.png",
     address: "https://tesla-clone.d2ml6p9nau3pde.amplifyapp.com/",
     description:
       "It is based on React.Js, Javascript, HTML, CSS. Designed to improve my coding skills.",
@@ -10,7 +10,7 @@ export const experienceData = [
   {
     id: "_2",
     title: "August 2021 - Ongoing",
-    landingImage: "../assests/images/exclusivepaintersImg.png",
+    landingImage: "../docs/images/exclusivepaintersImg.png",
     address: "http://www.exclusivepainters.co.nz/",
     description:
       "It is WordPress based website. Hosted on GoDaddy.com. Javascript, HTML, CSS, and Php are used. Also, it is running on a contract based.",
@@ -18,7 +18,7 @@ export const experienceData = [
   {
     id: "_3",
     title: "July 2020 - Ongoing",
-    landingImage: "../assests/images/nepalbazaarImg.png",
+    landingImage: "../docs/images/nepalbazaarImg.png",
     address: "http://www.nepalbazaar.co.nz/",
     description:
       "It is WordPress based Ecommerce website. Based on Javascript, HTML, CSS, and PHP. It is running on contract",
@@ -26,7 +26,7 @@ export const experienceData = [
   {
     id: "_4",
     title: " September 2022 - ongoing",
-    landingImage: "../assests/images/exclusivepainters1.jpg",
+    landingImage: "../docs/images/exclusivepainters1.jpg",
     address: "https://main.d1x7tfsl40h6kh.amplifyapp.com/displayJobs",
     description:
       "Still working on it:- It is based on React.Js, Javascript, HTML, CSS, bootstrap, lodash. Designed to improve WordPress based site and to provide more features to controls different job's activities.",
