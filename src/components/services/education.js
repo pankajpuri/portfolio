adf7ab362323131b15bdd2ad5258473f9e800eb6
Pankaj Puri <pankajpuri016@gmail.com>
2023-03-30 10:33:26 +1300

    edit profile

--- src/components/services/education.js
+++ src/components/services/education.js
@@ -5,12 +5,12 @@ const eductions = [
     center: "Auckland University of Technology - AUT",
     period: "Feb 2015 - Dec 2018",
   },
-  {
-    _id: "2sfsd",
-    education: "Developer Job Ready Programme",
-    center: "Industry Connect",
-    period: "Sep 2018 - Nov 2018",
-  },
+  // {
+  //   _id: "2sfsd",
+  //   education: "Developer Job Ready Programme",
+  //   center: "Industry Connect",
+  //   period: "Sep 2018 - Nov 2018",
+  // },
   {
     _id: "3sfsd",
     education: "Advance Software/Full Stack Developer",
